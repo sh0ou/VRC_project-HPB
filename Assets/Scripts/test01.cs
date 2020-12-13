56cc279dc8bc211d87d61accfaa99d6413f97e7d
shou <47475540+sh0ou@users.noreply.github.com>
2020-12-13 22:43:38 +0900

    初回コミット

---
+++ Assets/Scripts/test01.cs
@@ -0,0 +1,20 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using UnityEngine.UI;
+
+public class test01 : UdonSharpBehaviour
+{
+    [SerializeField]
+    private TextAsset textfile;
+    [SerializeField]
+    private Text titleText;
+    private string textData;
+
+    void Start()
+    {
+        textData = textfile.text;
+        titleText.text = textData;
+    }
+}
