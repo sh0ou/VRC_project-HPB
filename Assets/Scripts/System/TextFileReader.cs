ed4167934f2a85eceb2b51a4e8d1063aaf550b9b
shou <47475540+sh0ou@users.noreply.github.com>
2021-10-07 11:45:41 +0900

    v1.03
    ・Unityを2019.4.31f1に更新
    ・VRCSDKを2021.09.30.16.18に更新
    ・UdonSharpを0.20.3に更新
    
    ・看板のスクロール処理方法を変更
    ・お知らせ看板のテキスト処理を、平文ではなく、テキストファイルを読み込んで表示するように変更
    ・お知らせ看板がページ毎に表示されるように変更

---
+++ Assets/Scripts/System/TextFileReader.cs
@@ -0,0 +1,36 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace HPB
+{
+    /// <summary>
+    /// テキストファイルを文字列化し、格納します。
+    /// </summary>
+    public class TextFileReader : UdonSharpBehaviour
+    {
+        [SerializeField, Tooltip("テキストアセット")]
+        private TextAsset[] textfiles;
+
+        [TextArea] public string[] textStorages;
+
+        void Start()
+        {
+            textStorages = new string[textfiles.Length];
+            for (int i = 0; i < textfiles.Length; i++)
+            {
+                //Debug.Log("[<color=yellow>TextFileReader</color>]textconvert..." + i);
+                ConvertFileToString(i);
+            }
+        }
+
+        /// <summary>
+        /// TextAssetをStringに変換します
+        /// </summary>
+        public void ConvertFileToString(int i)
+        {
+            textStorages[i] = textfiles[i].text;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/PagingViewer.cs
@@ -0,0 +1,63 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using TMPro;
+
+namespace HPB
+{
+    /// <summary>
+    /// テキストをページ毎に表示させるスクリプト
+    /// </summary>
+    public class PagingViewer : UdonSharpBehaviour
+    {
+        [SerializeField] TextMeshProUGUI targetText;
+        [SerializeField] TextMeshProUGUI pageNumText_now;
+        [SerializeField] TextMeshProUGUI pageNumText_max;
+        [SerializeField] TextFileReader textFileReader;
+        int pageNum = 0;
+
+        void Start()
+        {
+            //Debug.Log("pagetext:" + textFileReader.textStorages.Length);
+            pageNum = textFileReader.textStorages.Length - 1;
+            targetText.text = textFileReader.textStorages[pageNum];
+            pageNumText_max.text = textFileReader.textStorages.Length.ToString();
+            pageNumText_now.text = pageNum + 1 + " / ";
+        }
+
+        /// <summary>
+        /// 次のページを表示
+        /// </summary>
+        public void NextPage()
+        {
+            if (pageNum == 0)
+            {
+                pageNum = textFileReader.textStorages.Length - 1;
+            }
+            else
+            {
+                pageNum--;
+            }
+            targetText.text = textFileReader.textStorages[pageNum];
+            pageNumText_now.text = pageNum + 1 + " / ";
+        }
+
+        /// <summary>
+        /// 前のページを表示
+        /// </summary>
+        public void BackPage()
+        {
+            if (pageNum == textFileReader.textStorages.Length - 1)
+            {
+                pageNum = 0;
+            }
+            else
+            {
+                pageNum++;
+            }
+            targetText.text = textFileReader.textStorages[pageNum];
+            pageNumText_now.text = pageNum + 1 + " / ";
+        }
+    }
+}
